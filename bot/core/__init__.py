9f3484c325fe69bb3b9566e080d9f85fca42d128
music-miko <voilet856@gmail.com>
2026-08-17 09:14:39 +0530

    Delete bot directory

--- bot/core/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2026 tusar404
-# Licensed under the MIT License.
-
-
-from .client import app
-from .config import config
-from .dirs import setup_directories
-from .mongo import mongo
--- bot/handlers/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2026 tusar404
-# Licensed under the MIT License.
-
-
-from .. import LOGGER
-from . import start, admin, callback, inline, search
-
-LOGGER.info("Handlers loaded -> start, admin, callback, inline, search")
