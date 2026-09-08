9a266b3044a1e4795d5fd7149f8e875a2f7f9bd3
music-miko <voilet856@gmail.com>
2026-09-09 02:13:41 +0530

    Delete bot directory

--- bot/core/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026 tusar404
-# Licensed under the MIT License.
-
-
-from .client import app
-from .clones import CloneManager, clones
-from .config import config
-from .dirs import setup_directories
-from .mongo import mongo
